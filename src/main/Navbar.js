145844a3a453dd4b9404f314fd56f4ce3e77db74
wonsuk lee <snlrnl213@gmail.com>
2022-07-04 17:16:47 +0900

    220704#start

---
+++ src/main/Navbar.js
@@ -0,0 +1,12 @@
+function Navbar(){
+    return <nav className="nav">
+        <a href="/" className="site-title">Site Name</a>
+        <ul>
+            <li>
+                <a href="/pricling">Pricing</a>
+                <a href="/about">About</a>
+            </li>
+        </ul>
+    </nav>
+}
+export default Navbar;
