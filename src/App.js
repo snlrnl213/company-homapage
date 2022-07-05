546a9aae05e8863e97311f2a07e01c6dc7844bb6
wonsuk lee <snlrnl213@gmail.com>
2022-07-05 15:22:32 +0900

    test

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div>
       <Router>
-        <Navbar />
+        {/* <Navbar /> */}
         <div className="container">
           <Switch>
             <Route path="/qna">
